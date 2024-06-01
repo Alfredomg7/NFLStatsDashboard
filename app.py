d6e4a36f28f6deec4d6cb70938a9124cc11cc39d
Alfredomg7 <alfredomg4000@gmail.com>
2024-06-01 13:02:06 -0700

    Add histogram and scatter plot charts, refactor to use bootstrap classes instead of custom styling for charts layout and standarize charts titles

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from config import rb_stats, wr_te_stats, qb_stats, team_stats, teams_color
 file_path = 'data/offense_yearly_data.csv'
 df = load_data(file_path)
 
-# Initialize dash app with bootstrap them
+# Initialize dash app with bootstrap theme
 load_figure_template('morph')
 app = Dash(__name__, external_stylesheets=[dbc.themes.MORPH])
 
@@ -66,8 +66,8 @@ app.layout = html.Div([
             ])
         ], className='col-12 col-xl-6 px-5'),
         html.Div([
-            html.H2('Team Stats by Year', className='text-center pb-3'),
-            dcc.Tabs(id='year-tabs', value=f'year-{max_year}', className='nav nav-pills', children=[
+            html.H2('Team Stats by Season', className='text-center pb-3'),
+            dcc.Tabs(id='bar-chart-year-tabs', value=f'year-{max_year}', className='nav nav-pills', children=[
                 dcc.Tab(label=str(year), value=f'year-{year}', className='nav-item') for year in range(min_year, max_year + 1)
             ]),
             html.Div([
@@ -79,7 +79,50 @@ app.layout = html.Div([
                     className='btn w-50 mb-3')
             ], className='p-3')
         ], className='col-12 col-xl-6 px-5')
-    ], style={'display': 'flex', 'flex-direction': 'row'})
+    ], className='row'),
+    html.Div([
+        html.Div([
+            html.H2('Offense Stats Distribution', className='text-center pb-3'),
+            dcc.Tabs(id='histogram-year-tabs', value=f'histogram-year-{max_year}', className='nav nav-pills', children=[
+                dcc.Tab(label=str(year), value=f'histogram-year-{year}', className='nav-item') for year in range(min_year, max_year + 1)
+            ]),
+            html.Div([
+                dcc.Graph(id='histogram-fantasy-points'),
+                dcc.Dropdown(
+                    id='histogram-stat-dropdown',
+                    options=[{'label': format_stat_name(stat), 'value': stat} for stat in team_stats],
+                    value='fantasy_points',
+                    className='btn w-50 mb-3')
+                ], className='p-3')
+        ], className='col-12 col-xl-6 p-3'),
+        html.Div([
+            html.H2('Offense Stats Correlation', className='text-center pb-3'),
+            dcc.Tabs(id='scatter-year-tabs', value=f'scatter-year-{max_year}', className='nav nav-pills', children=[
+                dcc.Tab(label=str(year), value=f'scatter-year-{year}', className='nav-item') for year in range(min_year, max_year + 1)
+            ]),
+            html.Div([
+                dcc.Graph(id='scatter-offensive-stats', className='w-100'),
+                html.Div([
+                    html.Div([
+                        dcc.Dropdown(
+                            id='scatter-x-stat-dropdown',
+                            options=[{'label': format_stat_name(stat), 'value': stat} for stat in team_stats],
+                            value='total_yards',
+                            className='btn w-100'
+                        ),
+                    ], className='col-12 col-xl-6 p-2'),
+                    html.Div([
+                        dcc.Dropdown(
+                            id='scatter-y-stat-dropdown',
+                            options=[{'label': format_stat_name(stat), 'value': stat} for stat in team_stats],
+                            value='total_tds',
+                            className='btn w-100'
+                        ),
+                    ], className='col-12 col-xl-6 p-2'),
+                ], className='row w-100')
+            ], className='p-3')
+        ], className='col-12 col-xl-6 p-3'),
+    ], className='row')
 ], className='container-fluid')
 
 # Player stats by position line charts callback
@@ -91,7 +134,7 @@ create_player_callback(app, 'QB', df_qb)
 # Team stats bar charts callback
 @app.callback(
     Output('team-bar-chart', 'figure'),
-    [Input('team-stat-dropdown', 'value'), Input('year-tabs', 'value')]
+    [Input('team-stat-dropdown', 'value'), Input('bar-chart-year-tabs', 'value')]
 )
 def update_team_bar_chart(selected_stat, selected_year):
     """
@@ -106,8 +149,67 @@ def update_team_bar_chart(selected_stat, selected_year):
     """
     selected_year = int(selected_year.split('-')[1])
     filtered_df = team_yearly_stats[team_yearly_stats['season'] == selected_year].sort_values(by=selected_stat, ascending=False)
-    fig = px.bar(filtered_df, x='team', y=selected_stat, title=f'{format_stat_name(selected_stat)} by Team {selected_year}', color='team', color_discrete_map=teams_color, template='morph')
+    fig = px.bar(filtered_df, x='team', y=selected_stat, title=f'{format_stat_name(selected_stat)} by Team {selected_year}', color='team', color_discrete_map=teams_color)
     fig.update_yaxes(title_text=format_stat_name(selected_stat))
+    return fig
+
+# Offense Stats Distribution Histogram callback
+@app.callback(
+    Output('histogram-fantasy-points', 'figure'),
+    [Input('histogram-stat-dropdown', 'value'), Input('histogram-year-tabs', 'value')]
+)
+def update_histogram(selected_stat, selected_year):
+    """
+    Update the histogram based on the stat selected in the dropdown and the year tab.
+
+    Args:
+        selected_stat (str): The selected statistic to display.
+        selected_year (str): The selected year tab.
+
+    Returns:
+        plotly.graph_objs._figure.Figure: The updated histogram figure.
+    """
+    selected_year = int(selected_year.split('-')[2])
+    filtered_df = df[df['season'] == selected_year]
+    filtered_df = filtered_df[filtered_df[selected_stat] > 0]
+    fig = px.histogram(filtered_df, x=selected_stat, nbins=20, color_discrete_sequence=['#20c997'], title=f'{format_stat_name(selected_stat)} Distribution {selected_year}')
+    fig.update_layout(xaxis_title=format_stat_name(selected_stat), yaxis_title='Frequency')
+    return fig
+
+# Offense Stats Correlation Scatter Plot callback
+@app.callback(
+    Output('scatter-offensive-stats', 'figure'),
+    [Input('scatter-x-stat-dropdown', 'value'),
+     Input('scatter-y-stat-dropdown', 'value'),
+     Input('scatter-year-tabs', 'value')
+    ]
+)
+def update_scatter_plot(x_stat, y_stat, selected_year):
+    """
+    Update the scatter plot based on the stats selected in the dropdowns and the year tab.
+
+    Args:
+        x_stat (str): The selected statistic for the x-axis.
+        y_stat (str): The selected statistic for the y-axis.
+        selected_year (str): The selected year tab.
+
+    Returns:
+        plotly.graph_objs._figure.Figure: The updated scatter plot figure.
+    """
+    selected_year = int(selected_year.split('-')[2])
+    filtered_df = team_yearly_stats[team_yearly_stats['season'] == selected_year]
+    filtered_df = filtered_df[(filtered_df[x_stat] > 0) & (filtered_df[y_stat] > 0)]
+    fig = px.scatter(
+        filtered_df,
+        x=x_stat,
+        y=y_stat,
+        color='team',
+        color_discrete_map=teams_color,
+        title=f'{format_stat_name(x_stat)} vs. {format_stat_name(y_stat)} {selected_year}', 
+        hover_data=['team']
+    )
+    fig.update_traces(marker_size=15)
+    fig.update_layout(xaxis_title=format_stat_name(x_stat), yaxis_title=format_stat_name(y_stat))
     return fig
 
 if __name__ == '__main__':
